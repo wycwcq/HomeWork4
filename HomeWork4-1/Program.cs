69fc5ced30677db86fadf9800774f48fb0750f2c
wycwcq <1193032407@qq.com>
2020-05-16 17:51:56 +0800

    添加项目文件。

---
+++ HomeWork4-1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_1
+{
+    class Program
+    {
+        public delegate void Hander(Clock clock);
+        public class Clock
+        {
+            //属性
+            public int nth, ntm, nts;
+            public int sth, stm, sts;
+            public Clock(int alh, int alm, int als)
+            {
+                sth = alh;
+                stm = alm;
+                sts = als;
+                nth = DateTime.Now.Hour;
+                ntm = DateTime.Now.Minute;
+                nts = DateTime.Now.Second;
+            }
+            public Hander tick, alarm;
+            public void StartClock()
+            {
+                int beginposition = 6;
+                Console.WriteLine("Clock Start!");
+                while (true)
+                {
+                    Console.SetCursorPosition(0, beginposition);
+                    if (nth == sth && ntm == stm && nts == sts)
+                    {
+                        Alarm(this);
+                        beginposition++;
+                    }
+                    else
+                    {
+                        Tick(this);
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                    nts++;
+                    if(nts == 60)
+                    {
+                        ntm++;
+                        nts = 0;
+                    }
+                    if(ntm == 60)
+                    {
+                        nth++;
+                        ntm = 0;
+                    }
+                    if(nth==24)
+                    {
+                        nth = 0;
+                    }
+                }
+            }
+        }
+        static void Alarm(Clock clock)
+        {
+            Console.WriteLine($"{clock.nth}:{clock.ntm}:{clock.nts}  Now_alarm");
+        }
+        static void Tick(Clock clock)
+        {
+            Console.WriteLine($"{clock.nth}:{clock.ntm}:{clock.nts}");
+        }
+
+        static void Main(string[] args)
+        {
+            Clock clock;
+            Console.WriteLine("Alarm time:(hour)");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Alarm time:(minute)");
+            string str2 = Console.ReadLine();
+            Console.WriteLine("Alarm time:(second)");
+            string str3 = Console.ReadLine();
+            try
+            {
+                int h = Int32.Parse(str1);
+                int m = Int32.Parse(str2);
+                int s = Int32.Parse(str3);
+                if(h < 0 || h >= 24 || m < 0 || m >= 60 || s < 0 || s >= 60)
+                {
+                    Console.WriteLine("时间输入有误!!!!!");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    clock = new Clock(h, m, s);
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("输入错误!");
+                Console.ReadKey();
+                return;
+            }
+            clock.tick += Tick;
+            clock.alarm += Alarm;
+            clock.StartClock();
+        }
+    }
+}
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get => head;
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+                Node<T> temp;
+                temp = head;
+                while (temp != tail)
+                {
+                    action(temp.Data);
+                    temp = temp.Next;
+                }
+                action(tail.Data);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+
+            Console.WriteLine("--------------------------------");
+            Action<string> action = delegate (string s) {
+                Console.WriteLine(s);
+            };
+
+            strList.ForEach(action);
+            Console.ReadKey();
+        }
+    }
+    
+
+}
